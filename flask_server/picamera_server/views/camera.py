2ce948458524545086ba05a5696b21b94aae91ca
Gaston Lucero <gaberrini@hotmail.com>
2020-04-23 16:59:41 -0300

    Development camera Class

--- flask_server/picamera_server/views/camera.py
+++ flask_server/picamera_server/views/camera.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, abort
+from picamera_server.views.utils.camera import Camera
+from flask import Blueprint, abort, render_template, Response
 from jinja2 import TemplateNotFound
 
 
@@ -7,7 +8,33 @@ camera = Blueprint('camera', __name__, template_folder='templates')
 
 @camera.route('/stream')
 def stream():
+    """
+    Path to return the front end for the video stream
+    :return:
+    """
     try:
         return render_template('camera/stream.html', section='stream')
     except TemplateNotFound:
         abort(404)
+
+
+def gen(_camera: Camera) -> bytes:
+    """
+    Generator used to create the multipart responses
+    :param _camera:
+    :return: --frame (image/jpeg) part of a multipart responses
+    """
+    while True:
+        frame = _camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@camera.route('/video_stream')
+def video_stream():
+    """
+    Endpoint used to feed the video stream with multipart responses
+    :return:
+    """
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
---
+++ flask_server/picamera_server/views/utils/camera.py
@@ -0,0 +1,57 @@
+import io
+import os
+import time
+from picamera_server.config import STATIC_FILES_PATH
+PI_CAMERA_IMPORTED = False
+try:
+    import picamera
+    PI_CAMERA_IMPORTED = True
+except ImportError:
+    pass
+
+
+class TestCamera(object):
+    """
+        An emulated camera implementation that streams a repeated sequence of
+        files [1.jpg, 2.jpg and 3.jpg] at a rate of one frame per second.
+    """
+
+    def __init__(self):
+        test_images_paths = [os.path.join(STATIC_FILES_PATH, 'test_images', number + '.jpg') for number in ['1',
+                                                                                                            '2',
+                                                                                                            '3']]
+        # Cache images to return
+        self.frames = [open(file, 'rb').read() for file in test_images_paths]
+
+    def get_frame(self) -> bytes:
+        """
+        Return a random test_image
+        :return: random test_image
+        """
+        return self.frames[int(time.time()) % 3]
+
+
+class PiCamera(object):
+
+    @staticmethod
+    def get_frame() -> bytes:
+        """
+        Return the current frame from the picamera
+        :return:
+        """
+        with picamera.PiCamera() as camera:
+            # let camera warm up
+            time.sleep(2)
+
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
+
+
+Camera = PiCamera if PI_CAMERA_IMPORTED else TestCamera
