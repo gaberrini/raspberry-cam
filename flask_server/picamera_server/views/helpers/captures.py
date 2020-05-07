3431d8c1c8aa5a7390802dff6758e3fcb0170eb3
Gaston Lucero <gaberrini@hotmail.com>
2020-05-06 21:06:14 -0300

    Get image from request

--- flask_server/picamera_server/views/helpers/captures.py
+++ flask_server/picamera_server/views/helpers/captures.py
@@ -1,4 +1,3 @@
-import base64
 from typing import List
 from picamera_server.models import CapturedImage
 
@@ -8,10 +7,8 @@ def get_captures_grids(captures: List[CapturedImage], grid_size: int = 4) -> Lis
     Return a list of captures grid to send to the templating for showing the captures
     in the UI_CAPTURES_PAGINATED page.
 
-    The images are going to be converted to base64 and decoded to utf-8 to remove the python b' when showing
-    bytes, this way we can show the images in the frontend directly
 
-    The grid item will be a dict with the "image" and the "date" of the capture
+    The grid item will be a dict with the "image" (relative_path) and the "date" of the capture
     :param captures:
     :param grid_size: Number of captures per grid
     :return:
@@ -23,7 +20,6 @@ def get_captures_grids(captures: List[CapturedImage], grid_size: int = 4) -> Lis
             grids.append(new_grid)
             new_grid = list()
 
-        image = base64.b64encode(capture.image).decode('utf-8')
-        new_grid.append({'image': image, 'date': capture.created_at})
+        new_grid.append({'image': capture.relative_path, 'date': capture.created_at})
     grids.append(new_grid)
     return grids
