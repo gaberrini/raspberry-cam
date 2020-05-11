5c71fdfe8c97de022704f57663f7ac99b15ab1a5
Gaston Lucero <gaberrini@hotmail.com>
2020-05-11 19:34:51 -0300

    Unix lines

--- scripts/run_server_debug.sh
+++ scripts/run_server_debug.sh
@@ -4,4 +4,5 @@ cd ./flask_server
 # This script must be runned after install_server_requirements.sh
 export FLASK_ENV=development
 export SERVER_PORT=8080
+
 python3 -m pipenv run python main.py -debug
